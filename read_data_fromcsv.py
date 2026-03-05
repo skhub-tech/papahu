041d81f2db9fe4f4c609cbaf1424f1d0d49b9d2c
Sahitya kanojiya <report.instamaxpro@gmail.com>
2026-03-05 22:39:29 +0530

    Update read_data_fromcsv.py

--- read_data_fromcsv.py
+++ read_data_fromcsv.py
@@ -1,24 +1,16 @@
-
-
 import pandas as pd
-
-sahitya = pd.read_csv("C:/Users/sahil/Downloads/dataset_dynamicprice.csv")
-
+sahitya = pd.read_csv("C:\/Users\/sahil\/Downloads\/dataset_dynamicprice.csv")
 sahitya["Profit"] = sahitya["Selling Price"] - sahitya["Cost Price"]
-
 print(sahitya.head())
-print("Summary statistics")
+print("\n")
 print(sahitya.describe())
-
-# Total Revenue (sum of selling prices)
-total_revenue = sahitya["Selling Price"].sum()
-
-# Total Profit
+print("\n")
 total_profit = sahitya["Profit"].sum()
+total_revanue = sahitya["Selling Price"].sum()
+avg = sahitya["Selling Price"].mean()
+most_sold = sahitya["Item Name"].value_counts().idxmax()
 
-# Average Price
-avg_price = sahitya["Selling Price"].mean()
-
-print("Total Revenue:", total_revenue)
-print("Total Profit:", total_profit)
-print("Average Price:", avg_price)
+print("total profit is ",total_profit)
+print("total revenue is ",total_revanue)
+print("Average is ",avg)
+print("most items are sold:",most_sold)
